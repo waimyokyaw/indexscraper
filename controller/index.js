5f40c4768f109ebce9e3a52406bb6c68946225ce
wai <wai@nexsource.net>
2018-02-04 16:02:38 +0700

    update api name and change route to scrapestocklist

--- controller/index.js
+++ controller/index.js
@@ -3,6 +3,6 @@ var server = express();
 var scraper = require('../helper/scraper');
 var router = express.Router();
 
-router.get('/scrapeindexlist', scraper.scrapeIndexList);
+router.get('/scrapestockindex', scraper.scrapeStockIndexList);
 
 module.exports = router;
